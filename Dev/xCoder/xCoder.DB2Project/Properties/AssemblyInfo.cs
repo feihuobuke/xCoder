3b4ceadd7f7187dc7f5a09f96926f06ccdf54177
SND\hermanxwong_cp <SND\hermanxwong_cp@2727b4ef-30a4-488d-b0fc-cb09fe5562a7>
2012-03-10 10:07:36 +0000

    Update assembly description

--- Dev/xCoder/xCoder.DB2Project/Properties/AssemblyInfo.cs
+++ Dev/xCoder/xCoder.DB2Project/Properties/AssemblyInfo.cs
@@ -16,34 +16,17 @@ using System.Runtime.InteropServices;
 // associated with an assembly.
 
 [assembly: AssemblyTitle("xCoder.DB2Project")]
-[assembly: AssemblyDescription("")]
-[assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("Microsoft")]
+[assembly: AssemblyDescription("xCoder Project is a solution for Auto-Generating project files, C# Code files from an existing DataBase.")]
+#if DEBUG
+[assembly: AssemblyConfiguration("Debug")]
+#else
+[assembly: AssemblyConfiguration("Release")]
+#endif
+[assembly: AssemblyCompany("xCoder Project Team @ CodePlex")]
 [assembly: AssemblyProduct("xCoder.DB2Project")]
-[assembly: AssemblyCopyright("Copyright © Microsoft 2012")]
-[assembly: AssemblyTrademark("")]
-[assembly: AssemblyCulture("")]
-
-// Setting ComVisible to false makes the types in this assembly not visible 
-// to COM components.  If you need to access a type in this assembly from 
-// COM, set the ComVisible attribute to true on that type.
-
+[assembly: AssemblyCopyright("Copyright © xCoder Project Team 2012")]
+[assembly: AssemblyTrademark("xCoder Project Team")]
+[assembly: AssemblyCulture("en-US")]
 [assembly: ComVisible(false)]
-
-// The following GUID is for the ID of the typelib if this project is exposed to COM
-
 [assembly: Guid("9e10a51e-b54c-49c7-b464-b0103586cddd")]
-
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version 
-//      Build Number
-//      Revision
-//
-// You can specify all the values or you can default the Build and Revision Numbers 
-// by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
-
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("0.1.*")]
